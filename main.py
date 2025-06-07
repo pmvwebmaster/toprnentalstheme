26a351974eb1b600746bfe4c0df7b3c6deceb3bb
Pablo Marques Viana <pabloviana.unisinos@gmail.com>
2025-06-07 15:54:47 -0300

    testing routes

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    in_stock: bool = True
 
 @app.get("/")
 def read_root():
     return {"message": "Olá, mundo!"}
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item": item}
